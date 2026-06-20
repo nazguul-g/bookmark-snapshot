0f036f9b1dd67c90f1150b0b056e8c8718bb3269
nazguul <youneshabbal1@gmail.com>
2026-06-20 10:37:33 +0100

    parsing research

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,5 +1,6 @@
-// Parses bookmarks for two different engines : gecko and chromium 
-// Initial anylysis found that gecko uses sqlite representation and chromuim uses json format 
+// Parses bookmarks for two different engines : gecko and chromium
+// Initial anylysis found that gecko uses sqlite representation and chromuim uses json format
 
-
-
+// in gecko based browsers, for safely parsing, copy the file into temp location , and opearte on it. cuz if the browser is running it might lock the DB
+// sqlite table where bookmarks resides is moz_bookmarks for mozzila browser
+// use stringlossy , some names are uncompatible with String
